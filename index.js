1a400188a0d27aeeacae9f35af5c22bf9554d5d9
shrutsureja <shrutsureja.code@gmail.com>
2024-03-03 13:06:08 +0530

    feat: add search function to fetch YouTube video  link for each song in the playlist
    Added new function 'search' and 'spotifytoyoutube' functions combine they search the spotify tracks on the youtube and take the first video and gives back the URL and title
    
    todo: Still need to error handling properly, if one youtube song is not fetched properly then entire process exists

--- index.js
+++ index.js
@@ -5,25 +5,30 @@ dotenv.config();
 
 import { PORT, client_id, redirect_uri } from './config.js';
 import { fetchUserTracks } from './spotifyFunctions.js';
+import { spotifytoyoutube } from './youtubeSearch.js';
 
 async function main() {
   console.log('Click here to give the code access to fetch your details from spotify:');
   console.log(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=user-read-private%20user-read-email%20playlist-read-private&state=34fFs29kd09`);
   open(`https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=user-read-private%20user-read-email%20playlist-read-private&state=34fFs29kd09`, {app : { name : 'firefox'}});
+  
   const allTracks = [];
+  
   // creating a temp server 
   const app = express();
+  
   app.get('/callback', async (req,res)=>{
     const code = req.query.code;
     try{
       const allTracks = await fetchUserTracks(code);
-      console.log('alltracks = ' + allTracks);
-      res.json(allTracks);
+      const result = await spotifytoyoutube(allTracks)
+      res.json(result);
       process.exit(0);
     }
     catch(e) {
       console.error('error in access token or the playlists', e);
       res.sendStatus(500);
+      process.exit(0)
     }
   });
   app.listen(PORT, console.log(`listting on ${PORT}`));
